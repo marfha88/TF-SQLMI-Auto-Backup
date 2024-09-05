ceefd18b0cdab465a6b396e61f231baa2ffe936a
Martin Fahlbeck <marfha88@icloud.com>
2024-09-05 12:46:47 +0200

    fix script

--- Powershell/sqlmi.ps1
+++ Powershell/sqlmi.ps1
@@ -59,7 +59,7 @@ foreach ($AzureSQLServerDataBase in $AzureSQLServerDataBases) {
     $longTermPolicy = Get-AzSqlInstanceDatabaseBackupLongTermRetentionPolicy -ResourceGroupName $resourcename -InstanceName $instancename -DatabaseName $($AzureSQLServerDataBase.Name)
 
     # Check if the Long Term Retention Policy is set
-    if ($longTermPolicy.MonthlyRetention -eq "P12M"){
+    if ($longTermPolicy.MonthlyRetention -eq "P12M"){ #Here we are checking if the Long Term Retention Policy is set to 12 months
         Write-Output "Long Term Retention Policy is set for database $($AzureSQLServerDataBase.Name) in SQLMI $instancename. Skipping Long Term Retention and Short Term Retention."
     } else {
         # If the Long Term Retention Policy is not set, proceed with setting the policies
@@ -71,34 +71,3 @@ foreach ($AzureSQLServerDataBase in $AzureSQLServerDataBases) {
         }
     }
 }
-
-
-
-
-
-<#
-foreach ($AzureSQLServerDataBase in $AzureSQLServerDataBases) {
-
-    # Hypothetical cmdlet to check if a Long Term Retention Policy is set
-$longTermPolicy = Get-AzSqlInstanceDatabaseBackupLongTermRetentionPolicy -ResourceGroupName $resourcename -InstanceName $instancename -DatabaseName $($AzureSQLServerDataBase.Name) 
-
-# Check if the Long Term Retention Policy is not set
-if ($longTermPolicy) {
-    Write-Output "Long Term Retention Policy is set for database $($AzureSQLServerDataBase.Name) in SQLMI $instancename. Skipping Set-AzSqlInstanceDatabaseBackupShortTermRetentionPolicy and Set-AzSqlInstanceDatabaseBackupLongTermRetentionPolicy."
-} else {
-    # If a Long Term Retention Policy is set, or if the policy status is unknown, proceed with setting the policies
-   
-#Short Term Retention Policy
-Set-AzSqlInstanceDatabaseBackupShortTermRetentionPolicy -ResourceGroupName $resourcename -InstanceName $instancename -DatabaseName $($AzureSQLServerDataBase.Name) -RetentionDays $RetentionDays
-#Long Term Retention Policy
-Set-AzSqlInstanceDatabaseBackupLongTermRetentionPolicy -WeeklyRetention “P$($WeeklyRetention)W” -MonthlyRetention “P$($MonthlyRetention)M” -InstanceName $instancename -DatabaseName $($AzureSQLServerDataBase.Name) -ResourceGroupName $resourcename
-}
-
-#Short Term Retention Policy
-#Set-AzSqlInstanceDatabaseBackupShortTermRetentionPolicy -ResourceGroupName $resourcename -InstanceName $instancename -DatabaseName $($AzureSQLServerDataBase.Name) -RetentionDays $RetentionDays
-#Long Term Retention Policy
-#Set-AzSqlInstanceDatabaseBackupLongTermRetentionPolicy -WeeklyRetention “P$($WeeklyRetention)W” -MonthlyRetention “P$($MonthlyRetention)M” -InstanceName $instancename -DatabaseName $($AzureSQLServerDataBase.Name) -ResourceGroupName $resourcename
-    }
-}
- #>
-
